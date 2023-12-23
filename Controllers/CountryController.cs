827e34616be683aaa01bef23d10a4795f6d9869c
DurDu54 <mucahitdurdu54@gmail.com>
2023-12-23 21:41:59 +0300

    Add project files.

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreDominationBootcampDay1.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetCountry()
+        {
+            string[] countries = new string[]
+            {
+                        "United States",
+                        "Canada",
+                        "United Kingdom",
+                        "Germany",
+                        "France",
+                        "Italy",
+                        "Japan",
+                        "Australia",
+                        "Brazil",
+                        "India",
+                        "China",
+                        "South Korea",
+                        "Mexico",
+                        "Russia",
+                        "South Africa",
+                        "Argentina",
+                        "Spain",
+                        "Netherlands",
+                        "Sweden",
+                        "Switzerland"
+            };
+
+            return countries;
+        }
+
+        [HttpGet("{userinput}")]
+        public string[] getCounryThatMuch(int userinput)
+        {
+            string[] totalcountry = new string[]
+            {
+                        "united states",
+                        "canada",
+                        "united kingdom",
+                        "germany",
+                        "france",
+                        "italy",
+                        "japan",
+                        "australia",
+                        "brazil",
+                        "india",
+                        "china",
+                        "south korea",
+                        "mexico",
+                        "russia",
+                        "south africa",
+                        "argentina",
+                        "spain",
+                        "netherlands",
+                        "sweden",
+                        "switzerland"
+            };
+            if (userinput < 0 || userinput > totalcountry.Length)
+            {
+
+                return new string[] { "invalid number" };
+            }
+            else
+            {
+                return totalcountry.TakeLast(userinput).ToArray();
+            }
+        }
+
+
+
+       [HttpGet("{userInput2}")]
+        public string[] SameCountryThatMuch(int userInput2)
+        {
+            string[] country = new string[]
+            {
+                "United States",
+                "Canada",
+                "United Kingdom",
+                "Germany",
+                "France",
+                "Italy",
+                "Japan",
+                "Australia",
+                "Brazil",
+                "India",
+                "China",
+                "South Korea",
+                "Mexico",
+                "Russia",
+                "South Africa",
+                "Argentina",
+                "Spain",
+                "Netherlands",
+                "Sweden",
+                "Switzerland"
+
+            };
+            if (userInput2 <= 0 || userInput2 > country.Length)
+            {
+                return new string[] { "Invalid number" };
+            }
+            else
+            {
+                var selected = country.ElementAt(userInput2 - 1);
+                var output = new string[userInput2];
+
+                for (int i = 0; i < userInput2; i++)
+                {
+                    output[i] = selected;
+                }
+
+                return output;
+            }
+
+        }
+    }
+}
